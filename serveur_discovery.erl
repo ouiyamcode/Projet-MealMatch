26ce2cff91a5dd20bef02a68ad9b41db121d3b16
ouiyamcode <williaminiziato54@gmail.com>
2025-05-14 19:32:38 +0200

    version avec IP ok

--- serveur_discovery.erl
+++ serveur_discovery.erl
@@ -11,13 +11,26 @@ start() ->
 loop(Socket) ->
   receive
     {udp, Socket, Host, Port, <<"mealmatch_discover">>} ->
-      IPString = inet_parse:ntoa(Host),
-      Response = <<"server:", IPString/binary>>,
-      io:format("📨 Requête de découverte reçue de ~p:~p~n", [IPString, Port]),
-      ok = gen_udp:send(Socket, Host, Port, Response),
+      IP = get_local_ip(),
+      IPString = inet_parse:ntoa(IP),
+      io:format("📨 Requête de découverte reçue, IP du serveur : ~s~n", [IPString]),
+      Response = <<"server:", (list_to_binary(IPString))/binary>>,
+      gen_udp:send(Socket, Host, Port, Response),
       io:format("✅ Réponse envoyée au client : ~p~n", [Response]),
       loop(Socket);
     Other ->
-      io:format("⚠️  Reçu un message inconnu : ~p~n", [Other]),
+      io:format("⚠️  Message inconnu reçu : ~p~n", [Other]),
       loop(Socket)
   end.
+
+get_local_ip() ->
+  {ok, Addrs} = inet:getifaddrs(),
+  IPs = [Addr || {_Iface, Props} <- Addrs,
+    {addr, Addr = {A,B,C,D}} <- Props,
+    A =/= 127],
+  case IPs of
+    [First | _] -> First;
+    [] -> {127,0,0,1}
+  end.
+
+
